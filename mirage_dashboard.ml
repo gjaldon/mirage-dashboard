8ddb6075bde19c3bc78bad46be1c0dc0dedf9469
rudenoise <rudenoise@gmail.com>
2016-03-13 16:10:45 +0000

    basic CLI application set-up

---
+++ mirage_dashboard.ml
@@ -0,0 +1,17 @@
+open Core.Std
+
+let spec =
+  let open Command.Spec in
+  empty
+
+let command =
+  Command.basic
+    ~summary: "A dashboard displaying useful data from the Mirage OS project and its related repositories."
+    ~readme: (fun () -> "More detailed info")
+    spec
+    (fun () ->
+       eprintf "coming soon...\n"
+    )
+
+let () =
+  Command.run ~version:"0.1" ~build_info:"RWO" command
