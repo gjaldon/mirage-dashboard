c4efd3bb19c08fb0ed71268f17a8292264ce7d0e
rudenoise <rudenoise@gmail.com>
2016-03-14 17:36:22 +0000

    make lwt usage consistent

--- src/github_wrapper.ml
+++ src/github_wrapper.ml
@@ -7,8 +7,10 @@ module M = Github.Monad
 exception Auth_token_not_found of string
 
 let get_auth_token_from_jar auth_id = 
-  lwt jar = Github_cookie_jar.init () in
-Github_cookie_jar.get jar auth_id >>= function
+  Github_cookie_jar.init ()
+  >>= fun jar ->
+    Github_cookie_jar.get jar auth_id
+    >>= function
     | Some(x) -> return x
     | None -> Lwt.fail (Auth_token_not_found "given id not in cookie jar")
 
@@ -16,3 +18,7 @@ let login ~cookie_name =
   match cookie_name with
   | "" -> Lwt.fail (Auth_token_not_found "must specify jar token id/cookie_name")
   | _ -> get_auth_token_from_jar cookie_name
+
+let get_release_for_repo ~token =
+    G.Release.for_repo ~token:token ~user:"mirage" ~repo:"mirage"
+
