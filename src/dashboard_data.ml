a655ee589156070f90c8b779ad8c77f16f9dd8c4
rudenoise <rudenoise@gmail.com>
2016-04-04 20:19:04 +0100

    work out mapping

--- src/dashboard_data.ml
+++ src/dashboard_data.ml
@@ -8,7 +8,14 @@ open Core.Std
 let repos_from_json =
     let repos_data = Yojson.Safe.from_file "data/repos.json" in
     let repo_list = repos_data |> Yojson.Safe.Util.to_list in
-    let filtered = List.map ~f: (fun item -> `Assoc [ item |> Yojson.Safe.Util.member "repo" ]) repo_list in
-    let final = `List filtered in
-    Yojson.Safe.pretty_to_string final
+    let filtered = (
+        `List
+        (
+            List.map
+            ~f: (fun item ->
+                Yojson.Safe.Util.member
+                "repo"
+                item)
+            repo_list)) in
+    Yojson.Safe.pretty_to_string filtered
 
