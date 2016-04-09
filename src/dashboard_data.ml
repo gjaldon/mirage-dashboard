2603f8b2605d139b52969ae4424c45c649d87c32
rudenoise <rudenoise@gmail.com>
2016-04-09 22:32:02 +0100

    begin turning json data into tuples to feed github requests

--- src/dashboard_data.ml
+++ src/dashboard_data.ml
@@ -5,19 +5,47 @@ open Core.Std
  * * #require "yojson";;
 *)
 
-let repos_from_json =
+let repo_list_from_json file_path =
+    let repos_data = Yojson.Safe.from_file file_path in
+    let repo_list = repos_data |> Yojson.Safe.Util.to_list in
+    (
+        List.map
+        ~f: (fun item -> (
+                (
+                    Yojson.Safe.Util.member
+                    "repo"
+                    item
+                ),
+                (
+                    (
+                        Yojson.Safe.Util.member
+                        "tags"
+                        item
+                    ) |> Yojson.Safe.Util.to_list
+                )
+            )
+        )
+        repo_list
+    )
+
+let repos_from_json_to_json =
     let repos_data = Yojson.Safe.from_file "data/repos.json" in
     let repo_list = repos_data |> Yojson.Safe.Util.to_list in
     let filtered = (
         `List
         (List.map
         ~f: (fun item ->
-            `Assoc [(
-                "repo",
-                (Yojson.Safe.Util.member
-                "repo"
-                item)
-            ); ("thing", `String "test")])
+            `Assoc [
+                (
+                    "repo",
+                    (
+                        Yojson.Safe.Util.member
+                        "repo"
+                        item
+                    )
+                );
+                ("thing", `String "test")
+            ])
         repo_list)) in
     Yojson.Safe.pretty_to_string filtered
 
--- src/dashboard_data.mli
+++ src/dashboard_data.mli
@@ -1,3 +1,6 @@
 (* read the local json file describing repos of interest *)
 
-val repos_from_json : string
+val repos_from_json_to_json : string
+
+val repo_list_from_json : string ->
+    (string * string list)
