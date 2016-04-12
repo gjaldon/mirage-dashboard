21d5da6f07864d54df82673503db8070e19f8473
rudenoise <rudenoise@gmail.com>
2016-04-12 09:35:30 +0100

    remove printing cofe

--- src/mirage_dashboard.ml
+++ src/mirage_dashboard.ml
@@ -4,8 +4,8 @@ open Lwt
 let quite_pretty_json s = Yojson.Safe.pretty_to_string (Yojson.Safe.from_string s)
 
 let first_str = function
-  | [] -> "{}"
   | hd :: _ -> hd
+  | _ -> "{}"
 
 
 let spec =
@@ -13,17 +13,6 @@ let spec =
   empty
     +> flag "-c" (required string) ~doc:"cookie github auth cookie, from git-jar"
 
-let get_release_and_print_async ~cookie_name ~user ~repo =
-    Github_wrapper.get_release
-      ~cookie_name
-      ~user
-      ~repo
-    >>= fun release ->
-    Github_wrapper.release_to_list release
-    >>= fun release_list ->
-    Github_wrapper.release_strings release_list
-    |> fun release_strings ->
-    Lwt_io.printf "%s\n" (quite_pretty_json (first_str release_strings))
 
 let get_release_async ~cookie_name ~user ~repo =
     Github_wrapper.get_release
@@ -49,21 +38,16 @@ let all_repos =
         repo_list
     )
 
-let print_all_repos ~cookie_name all_repos =
-  List.map
-    ~f:(
-      fun (user, repo) ->
-        get_release_and_print_async ~cookie_name ~user:user ~repo:repo
-    )
-    all_repos
-
 let get_all_repos ~cookie_name all_repos =
-  Lwt_list.map_s
+  Lwt_main.run 
     (
-      fun (user, repo) ->
-        get_release_async ~cookie_name ~user:user ~repo:repo
+      Lwt_list.map_s
+        (
+          fun (user, repo) ->
+            get_release_async ~cookie_name ~user:user ~repo:repo
+        )
+        all_repos
     )
-    all_repos
 
 let command =
   Command.basic
@@ -74,11 +58,11 @@ let command =
        Lwt_main.run (
          (* Lwt.join (print_all_repos ~cookie_name all_repos) *)
          get_all_repos ~cookie_name all_repos
-         >>= fun repos ->
+         |> fun repos ->
          String.concat ~sep:", " repos
          |> fun long_invalid_json_str ->
-         return (print_endline ("{" ^ long_invalid_json_str ^ "}"))
-         (* Lwt_io.printf "%s\n" long_invalid_json_str *)
+         (* return (print_endline ("{" ^ long_invalid_json_str ^ "}")) *)
+         Lwt_io.printf "%s\n" long_invalid_json_str
        )
     )
 
