de0382ab54a28b47bc81fcfeafd3691a916e57db
rudenoise <rudenoise@gmail.com>
2016-04-15 11:31:37 +0100

    remove unused functions from mlis

--- src/dashboard_data.mli
+++ src/dashboard_data.mli
@@ -1,8 +1,5 @@
 (* read the local json file describing repos of interest *)
 
-val repos_from_json_to_json : string
-
-val repo_list_from_json : string ->
-  ((Core.Std.String.t * Core.Std.String.t) * Yojson.Safe.json list) list
-
-val all_repos : repos_json_path:string -> (Core.Std.String.t * Core.Std.String.t) list
+val all_repos :
+  repos_json_path:string ->
+  (Core.Std.String.t * Core.Std.String.t) list
--- src/github_wrapper.mli
+++ src/github_wrapper.mli
@@ -1,9 +1,3 @@
-(* use a cookie from git jar to login to the gihub api*)
-
-val login :
-  cookie_name:string ->
-  Github_j.auth Lwt.t
-
 (* use a cookie from git jar and retrieve release info from github api *)
 
 val get_token :
