6e96f97ed5a5a20bc0511bfb644cb2f2a92bb50c
rudenoise <rudenoise@gmail.com>
2016-04-28 19:50:17 +0100

    use shell script to manage github rate limit

--- data/getMirageBot.js
+++ data/getMirageBot.js
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+set -e
+
 END=10
+
 for PAGE in $(seq 1 $END); do
-    curl https://api.github.com/users/miragebot/subscriptions?page=$PAGE | jq '[.[] | { repo: .full_name, tags: ["fromMirageBot"] }]' > data/mirage_bot_page$PAGE.json;
+    curl https://api.github.com/users/miragebot/subscriptions?page=$PAGE | jq '[.[] | { repo: .full_name, tags: ["fromMirageBot"] }]' > data/in/mirage_bot_page$PAGE.json;
+    ./mirage_dashboard.native -c mirage-dashboard -r data/in/mirage_bot_page$PAGE.json > data/out/page$PAGE.json
+    sleep 30
 done
 
-jq -s add data/mirage_bot_page*.json > data/repos_from_miragebot.json
+jq -s add data/out/page*.json > data/out/all.json
 
-rm data/mirage_bot_page*
