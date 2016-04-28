d31108a9129a73e43a987306997ffefd70762f4e
rudenoise <rudenoise@gmail.com>
2016-04-28 11:57:53 +0100

    store both lists of repos

--- data/getMirageBot.js
+++ data/getMirageBot.js
@@ -4,6 +4,6 @@ for PAGE in $(seq 1 $END); do
     curl https://api.github.com/users/miragebot/subscriptions?page=$PAGE | jq '[.[] | { repo: .full_name, tags: ["fromMirageBot"] }]' > data/mirage_bot_page$PAGE.json;
 done
 
-jq -s add data/mirage_bot_page*.json > data/repos.json
+jq -s add data/mirage_bot_page*.json > data/repos_from_miragebot.json
 
 rm data/mirage_bot_page*
