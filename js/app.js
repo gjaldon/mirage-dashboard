879528040ffe892e2d5736a5987c14cbf71c876b
rudenoise <rudenoise@gmail.com>
2016-04-29 10:27:06 +0100

    more div wrapping

--- js/app.js
+++ js/app.js
@@ -17,37 +17,40 @@
                         { class: 'jumbotron repo' },
                         ['h2', ['a', {href: 'https://github.com/' + item.user + '/' + item.repo }, item.repo]],
                         ['div',
-                            { class: 'media-body' },
-                            ['ul',
-                                ['li', 'By: ', ['a', {href: 'https://github.com/' + item.user}, item.user] ],
-                                [
-                                    'li',
-                                    'Contributors',
-                                    ['ul', (function () {
-                                        var k, rtn = [];
-                                        for (k in item.events) {
-                                            rtn.push(['li', ['a', { href: 'https://github.com/' + k }, k]])
-                                        }
-                                        return rtn;
-                                    }())]
-                                ],
-                                [
-                                    'li',
-                                    'Branches: ' + item.branches.length
+                            { class: 'media'},
+                            ['div',
+                                { class: 'media-body' },
+                                ['ul',
+                                    ['li', 'By: ', ['a', {href: 'https://github.com/' + item.user}, item.user] ],
+                                    [
+                                        'li',
+                                        'Contributors',
+                                        ['ul', (function () {
+                                            var k, rtn = [];
+                                            for (k in item.events) {
+                                                rtn.push(['li', ['a', { href: 'https://github.com/' + k }, k]])
+                                            }
+                                            return rtn;
+                                        }())]
+                                    ],
+                                    [
+                                        'li',
+                                        'Branches: ' + item.branches.length
+                                    ]
                                 ]
-                            ]
-                        ],
-                        ['div',
-                            { class: 'media-right' },
-                            [
-                                'h3',
-                                'Current Release:',
-                                ['em', [
-                                    item.current_release.name,
-                                    '-',
-                                    item.current_release.published ?
-                                        item.current_release.published : ''
-                                ].join(' ')]
+                            ],
+                            ['div',
+                                { class: 'media-right' },
+                                [
+                                    'h3',
+                                    'Current Release:',
+                                    ['em', [
+                                        item.current_release.name,
+                                        '-',
+                                        item.current_release.published ?
+                                            item.current_release.published : ''
+                                    ].join(' ')]
+                                ]
                             ]
                         ]
                     ];
