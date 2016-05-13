4a149633774fa364ab9f2ae160e276ad6a20008e
rudenoise <rudenoise@gmail.com>
2016-05-13 14:52:25 +0100

    start extracting repo info

--- src/get_info.ml
+++ src/get_info.ml
@@ -1,4 +1,21 @@
 open Lwt
 
-let get_info_for_repo ~token ~user ~repo =
-  return (Github.Repo.info ~token ~user ~repo ())
+let get_info_with_token ~token ~user ~repo =
+  let open Github.Monad in
+  run (
+      Github.Repo.info ~token ~user ~repo ()
+      >>~ fun info ->
+      let (descr = match info.repository_description with
+        | Some descr -> descr
+        | None -> ""
+      in return (`String descr)
+  )
+
+
+let get_info_with_cookie ~cookie_name ~user ~repo =
+  Github_wrapper.get_token ~cookie_name
+  >>= fun token ->
+  get_info_with_token ~token ~user ~repo
+
+let get_info ~cookie_name ~user ~repo =
+  get_info_with_cookie ~cookie_name ~user ~repo
