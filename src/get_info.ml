c173fe3a98586de7247a8590882aa4c925881b01
rudenoise <rudenoise@gmail.com>
2016-05-13 16:26:17 +0100

    begin refactoring to collapse repetative arge

--- src/get_info.ml
+++ src/get_info.ml
@@ -1,7 +1,8 @@
 open Lwt
 
-let get_info_with_token ~token ~user ~repo =
+let get_info_with_token (repo_with_token:Github_wrapper.repo_with_token) =
   let open Github.Monad in
+  let (token, user, repo) = repo_with_token in
   run (
       Github.Repo.info ~token ~user ~repo ()
       >>~ fun info ->
@@ -14,7 +15,7 @@ let get_info_with_token ~token ~user ~repo =
 let get_info_with_cookie ~cookie_name ~user ~repo =
   Github_wrapper.get_token ~cookie_name
   >>= fun token ->
-  get_info_with_token ~token ~user ~repo
+  get_info_with_token (token, user, repo)
 
 let get_info ~cookie_name ~user ~repo =
   get_info_with_cookie ~cookie_name ~user ~repo
--- src/github_wrapper.mli
+++ src/github_wrapper.mli
@@ -1,5 +1,11 @@
 (* use a cookie from git jar and retrieve release info from github api *)
 
+type cookie_name = string
+type user = string
+type repo = string
+type repo_with_cookie_name = (cookie_name * user * repo)
+type repo_with_token = (Github.Token.t * user * repo)
+
 val get_token :
   cookie_name:string ->
   Github.Token.t Lwt.t
